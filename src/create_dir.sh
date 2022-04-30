2fb5278fc4b2a38829f1d29fe097724f31e86afc
ChanYu1224 <kinkitunekin@gmail.com>
2022-04-30 10:34:03 +0900

    コンテスト用プロジェクトファイル一括作成シェルスクリプトの追加

---
+++ src/create_dir.sh
@@ -0,0 +1,8 @@
+mkdir contests/$1
+cat template.cpp > contests/$1/a.cpp
+cat template.cpp > contests/$1/b.cpp
+cat template.cpp > contests/$1/c.cpp
+cat template.cpp > contests/$1/d.cpp
+cat template.cpp > contests/$1/e.cpp
+cat template.cpp > contests/$1/f.cpp
+cat template.cpp > contests/$1/g.cpp
