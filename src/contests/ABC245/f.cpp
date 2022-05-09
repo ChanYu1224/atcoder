10ce729f38f5f5909c5e12089b82617d411d7545
yuta <kinkitunekin@gmail.com>
2022-05-09 11:01:01 +0900

    ABC245のE問題を進めた

--- src/contests/ABC245/f.cpp
+++ src/contests/ABC245/f.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
+#include <atcoder/scc>
 using namespace std;
+using namespace atcoder;
 
 #define _overload3(_1,_2,_3,name,...) name
 #define _rep(i,n) repi(i,0,n)
@@ -19,5 +21,43 @@ using namespace std;
 typedef long long ll;
 
 int main(){
+    int n,m; cin >> n >> m;
 
+    scc_graph graph(n);
+    vector<vector<int>> graph_list(n);
+
+    rep(i,m){
+        int u, v; cin >> u >> v;
+        u--;
+        v--;
+        graph.add_edge(u,v);
+        graph_list[u].push_back(v);
+    }
+
+    vector<vector<int>> scc = graph.scc(); //強連結成分を二次元ベクトルに変換
+    int k = scc.size(); //強連結成分の集合数
+    vector<int> index(n); //頂点がどの集合に属しているのかを格納
+
+    
+
+    rep(i,k){
+        for(auto v : scc[i]){
+            index[v] = i;
+        }
+    }
+
+    vector<int> dp(k);
+    int ans = 0;
+    for(int i = k-1; i >= 0; --i){
+        if(scc[i].size()==1){
+            for(auto v : graph_list[scc[i][0]]){
+                if(dp[index[v]]) dp[i] = 1;
+            }
+        }else{
+            dp[i] = 1;
+        }
+        if(dp[i]) ans += scc[i].size();
+    }
+
+    cout << ans << endl;
 }
